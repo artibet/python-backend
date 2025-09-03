39cd18af03784107eb3390d57f0b2e8200991271
Tsakiridis Sotiris <sotiris@serres.gr>
2025-09-03 12:56:36 +0300

    Update pagia scaffold

--- agriman/functions/pagia.py
+++ agriman/functions/pagia.py
@@ -11,8 +11,24 @@ def get_pagia(application_id):
 
   engine = get_engine()
 
+  # Remove this temporary template
+  doc = Document()
+  doc.add_heading('ΥΠΟ ΚΑΤΑΣΚΕΥΗ', level=0)
+  doc.add_paragraph('Το πρότυπο της πάγιας εντολής θα είναι διαθέσιμο σύντομα...')
+  buffer = BytesIO()
+  doc.save(buffer)
+  buffer.seek(0)
+
   # Continue here.....
 
 
 
   # Return the parsed pagia.docx (see symvasi.py)
+  # return as a downloadable file
+  return StreamingResponse(
+    buffer,
+    media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    headers={
+       "Content-Disposition": f"attachment; filename=pagia_{application_id}.docx"
+    }
+  )
